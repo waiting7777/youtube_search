13396556213da09b3020de173619f322974c2044
waiting7777 <waiting0819@msn.com>
2018-01-26 18:29:11 +0800

    add video list

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import YTSearch from 'youtube-api-search'
+
+import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+
+const API_KEY = 'AIzaSyCgL2IudtlwXRcWTb_Ov2-lVYPSp3wjdf8'
+
+
 
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = { videos: [] }
+
+    YTSearch({ key: API_KEY, term: 'joeman' }, (videos) => {
+      this.setState({ videos: videos })
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <SearchBar/>
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
---
+++ src/components/video_list_item.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class VideoListItem extends Component{
+    render() {
+        return <li>Video</li>
+    }
+}
+
+export default VideoListItem
